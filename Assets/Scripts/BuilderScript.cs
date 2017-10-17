44551ad86b360f9389f8256bec647a834cc045a9
CristianLuzon <cristianluzon@gmail.com>
2017-10-17 22:36:54 +0200

    Proyecto subido

---
+++ Assets/Scripts/BuilderScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuilderScript : MonoBehaviour {
+
+    public GameObject build;
+    public Vector3 buildingInMouse;
+
+    public GameObject cube;
+    public GameObject Cylinder;
+
+    public bool canCreateBuild = false;
+    public bool canPosisitioningBuild = false;
+
+    void Start()
+    {
+        buildingInMouse = new Vector3(0, 0, 0);
+
+        cube.SetActive(false);
+        Cylinder.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        if (canCreateBuild)
+        {
+            build.transform.position = buildingInMouse;
+        }
+        else
+        {
+            if (build != null) build.SetActive(false);
+        }
+
+        if (canPosisitioningBuild)
+        {
+            Instantiate(build, buildingInMouse, new Quaternion(0, 12, 0, 0));
+
+        }
+    }
+
+    public void GetBuldingMouse(Vector2 mousePos)
+    {
+        buildingInMouse = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+        buildingInMouse.y = 0;
+    }
+
+    /*public void PosisitioningBuild(bool canPositionBuild)
+    {
+        canCreateBuild = canPositionBuild;
+        build.SetActive(true);
+    }*/
+
+    public void SelectBuildingCube()
+    {
+        build = cube;
+        canCreateBuild = true;
+        build.SetActive(true);
+    }
+
+    public void SelectBuildingCylinder()
+    {
+        build = Cylinder;
+        canCreateBuild = true;
+        build.SetActive(true);
+    }
+
+    public void CreateBuild(bool canBuild)
+    {
+        if (canCreateBuild) canPosisitioningBuild = canBuild;
+    }
+}
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    public BuilderScript build;
+    public Vector2 mousePosition;
+    public Transform mouseTransform;
+
+    void Start () {
+		
+	}
+	
+	void Update ()
+    {
+        build.GetBuldingMouse(mousePosition);
+        mouseTransform.position = new Vector3(mousePosition.x, 0, mousePosition.y);
+
+    }
+
+    public void GetMouse(Vector2 newMousePos)
+    {
+        mousePosition = newMousePos;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Mouse is over GameObject.");
+    }
+}
