5ae39ef1dab6413199a12d516aeaf1b810cea59f
Jordi <jjorba95@gmail.com>
2017-10-21 12:51:29 +0200

    Fixed Collision checkers && Red collision Color
    @adri102  @cristianluzon  @korobli  worked on this update

--- Assets/Scripts/BuilderScript.cs
+++ Assets/Scripts/BuilderScript.cs
@@ -8,11 +8,14 @@ public class BuilderScript : MonoBehaviour {
     public Vector3 buildingInMouse;
 
     public GameObject cube;
-    public GameObject Cylinder;
+    public GameObject cylinder;
+    public Material collisionZone;
+    public Material cubeMaterial;
+    public Material cylinderMaterial;
 
     public Transform collisionChecker;
     public Vector3 colliderHalfSize;
-    public LayerMask Buildings;
+    public LayerMask layerBuild;
 
     public bool canCreateBuild = false;
     public bool canPosisitioningBuild = false;
@@ -25,7 +28,7 @@ public class BuilderScript : MonoBehaviour {
         buildingInMouse = new Vector3(0, 0, 0);
 
         cube.SetActive(false);
-        Cylinder.SetActive(false);
+        cylinder.SetActive(false);
     }
 
     void Update()
@@ -33,23 +36,19 @@ public class BuilderScript : MonoBehaviour {
         if (canCreateBuild)
         {
             build.transform.position = buildingInMouse;
+            collisionZone = build.GetComponent<Renderer>().material;
+            ChangeColorOnCollision();
         }
         else
         {
-            if (build != null) build.SetActive(false);
-        }
-
-        if (canPosisitioningBuild)
-        {
-            Instantiate(build, buildingInMouse, new Quaternion(0, 12, 0, 0));
-
+            if (build != null) build.SetActive(false); 
         }
     }
 
     private void FixedUpdate()
     {
-        Collider[] hitCollider = Physics.OverlapBox(collisionChecker.position, colliderHalfSize, Quaternion.identity, Buildings);
-        if (hitCollider.Length >= 0) buildingColliding = true;
+        Collider[] hitCollider = Physics.OverlapBox(collisionChecker.position, colliderHalfSize, Quaternion.identity, layerBuild);
+        if (hitCollider.Length != 0) buildingColliding = true;
         else buildingColliding = false;
     }
 
@@ -63,22 +62,32 @@ public class BuilderScript : MonoBehaviour {
     {
         DesactiveOriginalBuilding();
         build = cube;
+        collisionChecker = cube.transform;
         canCreateBuild = true;
         build.SetActive(true);
-        build.layer = 9;
     }
 
     public void SelectBuildingCylinder()
     {
         DesactiveOriginalBuilding();
-        build = Cylinder;
+        build = cylinder;
+        collisionChecker = cylinder.transform;
         canCreateBuild = true;
         build.SetActive(true);
     }
 
-    public void CreateBuild(bool canBuild)
+    public void CreateBuild()
     {
-        if (canCreateBuild && !buildingColliding) canPosisitioningBuild = canBuild;
+        if (canCreateBuild && buildingColliding == false) canPosisitioningBuild = true;
+
+        if (canPosisitioningBuild)
+        {
+            ChangeMaterialClone();
+            build.layer = 9;
+            Instantiate(build, buildingInMouse, new Quaternion(0, 12, 0, 0));
+            build.layer = 8;
+            ChangeMaterialOriginal();
+        }
     }
 
     public void CantBuild(bool variable)
@@ -89,9 +98,32 @@ public class BuilderScript : MonoBehaviour {
     public void DesactiveOriginalBuilding()
     {
         cube.SetActive(false);
-        Cylinder.SetActive(false);
+        cylinder.SetActive(false);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(collisionChecker.position, colliderHalfSize * 2);
+    }
+
+    private void ChangeColorOnCollision()
+    {
+        if (buildingColliding) collisionZone.color = Color.red;
+        else collisionZone.color = Color.white;
+    }
+
+    private void ChangeMaterialClone()
+    {
+        if (build.tag == "Cube") build.GetComponent<Renderer>().material = cubeMaterial;
+        else if (build.tag == "Cylinder") build.GetComponent<Renderer>().material = cylinderMaterial;
+    }
+
+    private void ChangeMaterialOriginal()
+    {
+        if (build.tag == "Cube") build.GetComponent<Renderer>().material = cube.GetComponent<Renderer>().material;
+        else if (build.tag == "Cylinder") build.GetComponent<Renderer>().material = cylinder.GetComponent<Renderer>().material;
+    }
 
     /*private void OnCollisionEnter(Collision collision)
     {
@@ -108,6 +140,4 @@ public class BuilderScript : MonoBehaviour {
             buildingColliding = false;
         }
     }*/
-
-
 }
