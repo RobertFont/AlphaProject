caf5c63ed3a1673d6596a5c0a9297ee18f4d18de
CristianLuzon <cristianluzon@gmail.com>
2017-11-10 21:06:52 +0100

    Fixed setting menu

---
+++ Assets/Scripts/SettingsButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsButton : MonoBehaviour {
+
+	public void Resolution(int res)
+    {
+        if (res == 1) Screen.SetResolution(1920, 1080, Screen.fullScreen);
+        else if (res == 2) Screen.SetResolution(1280, 720, Screen.fullScreen);
+        else if (res == 3) Screen.SetResolution(800, 600, Screen.fullScreen);
+    }
+
+    public void FullScreenMode()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void Quality(int value)
+    {
+       QualitySettings.SetQualityLevel(value);
+    }
+
+    public class ExampleClass : MonoBehaviour
+    {
+        public int qualityLevel = QualitySettings.GetQualityLevel();
+    }
+}
