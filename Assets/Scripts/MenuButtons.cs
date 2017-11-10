a944e4e52acbe59250bf8b13f858a6c623a8d802
CristianLuzon <cristianluzon@gmail.com>
2017-11-10 20:45:19 +0100

    Added menu scene

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    public void Play(int index, float volume, float pitch)
+    {
+        AudioSource audioSource = this.gameObject.AddComponent<AudioSource>();
+        audioSource.clip = clips[index];
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        audioSource.Play();
+        Destroy(audioSource, clips[index].length);
+    }
+
+}
