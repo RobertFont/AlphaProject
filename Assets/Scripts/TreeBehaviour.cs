be0a7cbff78c7e65498608921870d0127db7ad7c
Adri102 <adri.arranz102@gmail.com>
2017-11-28 00:43:01 +0100

    Added trees
    Added the trees to the test scene, now the trees collide with the buildings

---
+++ Assets/Scripts/TreeBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TreeBehaviour : MonoBehaviour {
+
+    public Vector3 treeSize;
+
+    public float random;
+   
+
+    public GameObject treeTop;
+    public GameObject treeMid;
+    public GameObject treeBot;
+
+    // Use this for initialization
+    void Start ()
+    {
+        random = Random.RandomRange(0.6f, 1.2f);
+        treeSize = treeTop.transform.localScale;
+     
+        treeSize = new Vector3(random, Random.RandomRange(0.6f, 1.4f), random);
+     
+        treeTop.transform.localScale = treeSize;
+        treeMid.transform.localScale = treeSize;
+        treeBot.transform.localScale = treeSize;
+    }
+	
+}
