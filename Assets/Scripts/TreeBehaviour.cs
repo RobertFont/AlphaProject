22fcbe413702c39db730d00ad6f1427929aad739
Adria <adri.arranz102@gmail.com>
2017-11-28 19:02:47 +0100

    update from class

--- Assets/Scripts/TreeBehaviour.cs
+++ Assets/Scripts/TreeBehaviour.cs
@@ -8,26 +8,27 @@ public class TreeBehaviour : MonoBehaviour {
     private Vector3 treeSize;
     private Vector3 treeRotation;
     private float random;
-    public GameObject treeTop;
-    public GameObject treeMid;
-    public GameObject treeBot;
+    public Transform tree;
+    //public GameObject treeMid;
+    //public GameObject treeBot;
 
     // Use this for initialization
     void Start ()
     {
+        tree = this.transform;
         random = Random.RandomRange(0.7f, 1.2f);
-        treeSize = treeTop.transform.localScale;
+        treeSize = tree.localScale;
         
         treeSize = new Vector3(random, Random.RandomRange(0.7f, 1.4f), random);
         treeRotation = new Vector3(0, Random.RandomRange(0.0f, 360.0f), 0);
 
-        treeTop.transform.localScale = treeSize;
-        treeMid.transform.localScale = treeSize;
-        treeBot.transform.localScale = treeSize;
+        tree.localScale = treeSize;
+        //treeMid.transform.localScale = treeSize;
+        //treeBot.transform.localScale = treeSize;
 
-        treeTop.transform.Rotate(treeRotation);
-        treeMid.transform.Rotate(treeRotation);
-        treeBot.transform.Rotate(treeRotation);
+        tree.transform.Rotate(treeRotation);
+        //treeMid.transform.Rotate(treeRotation);
+        //treeBot.transform.Rotate(treeRotation);
     }
 	
 }
