5f5c1eb90d7683c75a48e600f0d30759b56659b7
Adri102 <adri.arranz102@gmail.com>
2017-11-28 10:26:05 +0100

    Added a random rotation for the trees

--- Assets/Scripts/TreeBehaviour.cs
+++ Assets/Scripts/TreeBehaviour.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class TreeBehaviour : MonoBehaviour {
 
     public Vector3 treeSize;
+    public Vector3 treeRotation;
 
     public float random;
-   
+    //public float randomRotation;
+
 
     public GameObject treeTop;
     public GameObject treeMid;
@@ -18,13 +20,20 @@ public class TreeBehaviour : MonoBehaviour {
     void Start ()
     {
         random = Random.RandomRange(0.6f, 1.2f);
+        //randomRotation = Random.RandomRange(0.0f, 360.0f);
         treeSize = treeTop.transform.localScale;
-     
+        
+
         treeSize = new Vector3(random, Random.RandomRange(0.6f, 1.4f), random);
-     
+        treeRotation = new Vector3(0, Random.RandomRange(0.0f, 360.0f), 0);
+
         treeTop.transform.localScale = treeSize;
         treeMid.transform.localScale = treeSize;
         treeBot.transform.localScale = treeSize;
+
+        treeTop.transform.Rotate(treeRotation);
+        treeMid.transform.Rotate(treeRotation);
+        treeBot.transform.Rotate(treeRotation);
     }
 	
 }
