4e6860d52e3bc617c4852b2b01b5411826765964
Jordi <jjorba95@gmail.com>
2017-11-30 00:26:13 +0100

    Uploaded navigation mesh Awesome

---
+++ Assets/Scripts/NavigateTesting/PeasantBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PeasantBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [Header("Path")]
+    public Transform[] points;
+    private int pathIndex = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance + 0.1f)
+        {
+            pathIndex++;
+            if (pathIndex >= points.Length) pathIndex = 0;
+        }
+
+        agent.SetDestination(points[pathIndex].position);
+    }
+}
