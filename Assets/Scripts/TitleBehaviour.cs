f00c28536ee97c18df8dd5f15aa0d8190f57745b
CristianLuzon <cristianluzon@gmail.com>
2017-12-07 11:44:36 +0100

    Changed animation
    Logo animation
    Title animation

--- Assets/Scripts/TitleBehaviour.cs
+++ Assets/Scripts/TitleBehaviour.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class TitleBehaviour : MonoBehaviour
 {
     LevelLogic level;
+    public Image titleLogo;
+    public Color titleColor;
+    public float timeCounter = 0;
 
     void Start()
     {
         level = GameObject.Find("LevelManager").GetComponent<LevelLogic>();
+        titleColor = titleLogo.color;
     }
 	
 	void Update()
     {
-		
-	}
+        if (timeCounter < 0.5f)
+        {
+            timeCounter += Time.deltaTime;
+        }
+        else
+        {
+            timeCounter = 2;
+
+            if (titleColor.a >= 1.0f)
+            {
+                titleColor.a = 1.0f;
+            }
+            else if (titleColor.a < 1.0f)
+            {
+                titleColor.a += 0.01f;
+            }
+            titleLogo.color = titleColor;
+        }
+    }
 
     public void Continue()
     {
