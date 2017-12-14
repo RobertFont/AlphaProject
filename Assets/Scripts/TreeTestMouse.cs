4d01deb4a766242a4af068eb3c3cbfa4e5b73197
- <->
2017-12-14 18:24:25 +0100

    Update optimization

---
+++ Assets/Scripts/TreeTestMouse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTestMouse : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("DOWN");
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("UP AS BUTTON");
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("UP");
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("DRAG");
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("ENTER");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("EXIT");
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("OVER");
+    }  
+}
