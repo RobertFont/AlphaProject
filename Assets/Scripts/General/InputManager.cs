e499f025111a6a1fca9b3d80cc893d55344e0696
CristianLuzon <cristianluzon@gmail.com>
2018-01-21 18:15:52 +0100

    Implemented support to XBox360 controller

--- Assets/Scripts/General/InputManager.cs
+++ Assets/Scripts/General/InputManager.cs
@@ -36,7 +36,7 @@ public class InputManager : MonoBehaviour {
         if (Input.GetButtonDown("Pause")) TogglePause();
         if (paused) return;
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetButtonDown("Cancel"))
         {
             if (!builder.canCreateBuild) uiConstruction.StopConstruction();
             uiTrigger.DeselectBuilding();
@@ -47,6 +47,12 @@ public class InputManager : MonoBehaviour {
 
         inputMouse.x = Input.GetAxis("Mouse X");
         inputMouse.y = Input.GetAxis("Mouse Y");
+
+        inputMouse.x = Input.GetAxis("Right Joystick X");
+        inputMouse.y = Input.GetAxis("Right Joystick Y");
+
+        mousePosition.x += inputMouse.x;
+        mousePosition.y += inputMouse.y;
 
         mousePosition.x = Input.mousePosition.x;
         mousePosition.y = Input.mousePosition.y;
@@ -59,25 +65,33 @@ public class InputManager : MonoBehaviour {
         }
         
         #region Speed
-        if (Input.GetButtonDown("Speed1")) Time.timeScale = 1.0f;
-        if (Input.GetButtonDown("Speed2")) Time.timeScale = 1.5f;
-        if (Input.GetButtonDown("Speed3")) Time.timeScale = 2.0f;
+        if (Input.GetButtonDown("Speed1") || Input.GetAxis("Speed1 and 3 Controller") < -0.05f) Time.timeScale = 1.0f;
+        if (Input.GetButtonDown("Speed2") || Input.GetAxis("Speed2 Controller") > 0.05f) Time.timeScale = 1.5f;
+        if (Input.GetButtonDown("Speed3") || Input.GetAxis("Speed1 and 3 Controller") > 0.05f) Time.timeScale = 2.0f;
         #endregion
 
+        Debug.Log(Time.timeScale);
+
         #region Rotate
-        if (Input.GetKey(KeyCode.Q)) rotate = 1;
-        else if (Input.GetKey(KeyCode.E)) rotate = -1;
+        /*if (Input.GetKey(KeyCode.Q)) rotate = 1;
+        else if (Input.GetKey(KeyCode.E)) rotate = -1;*/
+        if (Input.GetButtonDown("Rotate+")) rotate = 1;
+        else if (Input.GetButtonDown("Rotate-")) rotate = -1;
 
         if (rotate >= -0.015 && rotate <= 0.015f) rotate = 0;
         else if (rotate < -0.01f) rotate += Time.deltaTime;
         else if (rotate > 0.01f) rotate -= Time.deltaTime;
         #endregion
-
         #region Camera Zoom
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
             mainCamera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed; //Change values according to your requirements
         }
+        if (Input.GetAxis("Mouse ScrollWheel Controller") != 0)
+        {
+            mainCamera.fieldOfView -= Input.GetAxis("Mouse ScrollWheel Controller") * zoomSpeed; //Change controller values according to your requirements
+        }
+
 
         if (mainCamera.fieldOfView >= 60) mainCamera.fieldOfView = 60;
         else if (mainCamera.fieldOfView <= 30) mainCamera.fieldOfView = 30;
