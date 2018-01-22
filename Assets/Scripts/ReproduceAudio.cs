e391be7106e7552a4de6bc8dfb2b6afd09369956
- <->
2018-01-22 19:02:34 +0100

    Updated menu and added fx sounds

---
+++ Assets/Scripts/ReproduceAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproduceAudio : MonoBehaviour
+{
+    PlaySound play;
+    public int audio;
+    public bool activeOnEnable;
+    public bool disableAfterPlaySound;
+
+    private void Start()
+    {
+        if (play == null) play = GameObject.Find("LevelManager").GetComponent<PlaySound>();
+    }
+
+    void OnEnable()
+    {
+        if(play == null) play = GameObject.Find("LevelManager").GetComponent<PlaySound>();
+        if(activeOnEnable)
+            Activated();
+    }
+
+    public void Activated()
+    {
+        play.PlayFX(audio, 1.0f, false);
+        if(disableAfterPlaySound) gameObject.SetActive(false);
+    }
+}
