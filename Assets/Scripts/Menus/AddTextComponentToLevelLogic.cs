32202d5ad37023752cab6ed2be4e95717ae20a59
- <->
2018-01-29 19:17:22 +0100

    TitleScene
    Implemeneted font to option menu
    Created a new script to storage all text components

---
+++ Assets/Scripts/Menus/AddTextComponentToLevelLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddTextComponentToLevelLogic : MonoBehaviour
+{
+    public List<Text> textComponents = new List<Text>();
+
+	void Start ()
+    {
+	}
+	
+	void Update ()
+    {
+	}
+
+    public List<Text> GetTextList()
+    {
+        return textComponents;
+    }
+}
