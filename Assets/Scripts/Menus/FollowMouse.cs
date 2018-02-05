7318b199a5e55810c2d537616dc6f11add6200ff
CristianLuzon <cristianluzon@gmail.com>
2018-02-05 22:18:35 +0100

    Implemented little things
    Mouse texture to follow mouse
    Added game icon

---
+++ Assets/Scripts/Menus/FollowMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FollowMouse : MonoBehaviour
+{
+    Transform cursorPos;
+    public Canvas myCanvas;
+    float with;
+    float hight;
+
+	void Start ()
+    {
+        cursorPos = transform;
+        with = GetComponent<RectTransform>().sizeDelta.x;
+        hight = GetComponent<RectTransform>().sizeDelta.y;
+    }
+	void FixedUpdate ()
+    {
+        cursorPos.localPosition = Input.mousePosition - (myCanvas.transform.localPosition);
+    }
+}
