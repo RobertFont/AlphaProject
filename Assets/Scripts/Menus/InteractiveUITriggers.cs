a697798c3cd977d87902b3aa6d80d0da75c97402
CristianLuzon <cristianluzon@gmail.com>
2018-02-08 19:10:08 +0100

    Created script To do interactive UI elements

---
+++ Assets/Scripts/Menus/InteractiveUITriggers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractiveUITriggers : MonoBehaviour
+{
+    public void ChangeImage(Sprite texture)
+    {
+        GetComponent<Image>().sprite = texture;
+	}
+}
