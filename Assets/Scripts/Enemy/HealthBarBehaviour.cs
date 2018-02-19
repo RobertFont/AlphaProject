81f57c3f0659362860e0006efeda6c3a4afb3e64
Adria <adri.arranz102@gmail.com>
2018-02-19 18:15:28 +0100

    Added Billboard texture to the enemy healthBar

---
+++ Assets/Scripts/Enemy/HealthBarBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarBehaviour : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    private void Start()
+    {
+        m_Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
