3f38e012a8ece9b688380cc6f032c2ec4269a0ca
Adri <adri.arranz102@gmail.com>
2018-03-19 18:35:22 +0100

    Added infoButtons script

---
+++ Assets/Scripts/MouseOverBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseOverBehaviour : MonoBehaviour {
+
+    public GameObject info;
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Mouse enter");
+        info.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("Mouse exit");
+        info.SetActive(false);
+    }
+}
