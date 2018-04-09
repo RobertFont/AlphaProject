7901ad23c00986205e15af3a7ac6204d1f426666
CristianLuzon <cristianluzon@gmail.com>
2018-04-09 21:23:23 +0200

    Little update
    Created and implemented automatic color change to the text.
    Implemented castle mesh and collider adjustment.

---
+++ Assets/Scripts/TextLogics/TextChangeColor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TextChangeColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public Color defaultColor;
+    public Color OverColor;
+    public Color pressedColor;
+    Text textTarget;
+
+    void Start ()
+    {
+        textTarget = GetComponentInChildren<Text>();
+        defaultColor = textTarget.color;
+        OverColor = Color.yellow;
+        pressedColor = Color.yellow;
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textTarget.color = OverColor;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        textTarget.color = defaultColor;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        textTarget.color = pressedColor;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        textTarget.color = OverColor;
+    }
+}
