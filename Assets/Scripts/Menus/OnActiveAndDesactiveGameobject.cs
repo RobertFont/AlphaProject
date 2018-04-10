7b450233c42fd1e80c2cc0650fa93c90449dbbfb
CristianLuzon <cristianluzon@gmail.com>
2018-04-10 17:09:43 +0200

    Fixed menu bug with eventSystems

--- Assets/Scripts/Menus/OnActiveAndDesactiveGameobject.cs
+++ Assets/Scripts/Menus/OnActiveAndDesactiveGameobject.cs
@@ -1,20 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class OnActiveAndDesactiveGameobject : MonoBehaviour
 {
     public GameObject desactiveThis;
     public GameObject activeThis;
-    
-    void Start ()
-    {
-	}
 
     public void OnEnable()
     {
+        Debug.Log("Animation start");
         activeThis.SetActive(true);
         desactiveThis.SetActive(false);
         gameObject.SetActive(false);
+        Debug.Log("Animation end");
     }
 }
