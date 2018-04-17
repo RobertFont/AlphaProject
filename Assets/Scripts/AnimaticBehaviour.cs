309958de7afd58e42acc0fc9f3f324202d73974b
Jordi <jjorba95@gmail.com>
2018-04-17 17:14:47 +0200

    Updated animatic scene

---
+++ Assets/Scripts/AnimaticBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaticBehaviour : MonoBehaviour
+{
+    public GameObject trees;
+
+    // Use this for initialization
+    void Start ()
+    {
+        for(int i = 0; i < trees.transform.childCount; i++)
+        {
+            if(trees.transform.GetChild(i).GetComponent<TreeProps>() != null) trees.transform.GetChild(i).GetComponent<TreeProps>().MyStart();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
